6dba6c6d60dcdd301d99e02be45d1379883ef961
unknown <nainiayouness@gmail.com>
2021-04-19 16:53:37 +0000

    making the table component a class one

--- Client/src/components/Other/Table/Rows.js
+++ Client/src/components/Other/Table/Rows.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Row from './Row'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Table } from 'reactstrap'
@@ -6,30 +6,43 @@ import "../../../css/table.css"
 import   {withRouter} from 'react-router-dom'
 
 
-function Rows(props) {
-  const {rows,theHeaders}=props
-  return (
-    <>
-        <Table striped bordered hover responsive className="position" style={{width:'100 %'}} >
-            <thead>
-              <tr style={{backgroundColor:"#ff9333"}}>
-                {
-                  theHeaders.map((header,index)=>{
-                    return <th key={index}>{header}</th>
-                  })
-                }
-              </tr>
-            </thead>
-            <tbody>
-                {
-                    rows.map((appObject,index)=>{
-                      return <Row  key={index} packagename={appObject.packagename}  devName={appObject.developername}  rank={appObject.rank} logo={appObject.imageurl} title={appObject.title} download={appObject.downloadtraced} reviews={appObject.ratingcount} rating={appObject.starrating}></Row>
-                    })
-                }
-            </tbody>
-        </Table>
-    </>
-  )
+
+
+class Rows extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       
+    }
+  }
+  
+  render() {
+    const {rows,theHeaders}=this.props
+    return (
+        <>
+            <Table striped bordered hover responsive className="position" style={{width:'100 %'}} >
+                <thead>
+                  <tr style={{backgroundColor:"#ff9333"}}>
+                    {
+                      theHeaders.map((header,index)=>{
+                        return <th key={index}>{header}</th>
+                      })
+                    }
+                  </tr>
+                </thead>
+                <tbody>
+                    {
+                        rows.map((appObject,index)=>{
+                          return <Row  key={index} packagename={appObject.packagename}  devName={appObject.developername}  rank={appObject.rank} logo={appObject.imageurl} title={appObject.title} download={appObject.downloadtraced} reviews={appObject.ratingcount} rating={appObject.starrating}></Row>
+                        })
+                    }
+                </tbody>
+            </Table>
+        </>
+    )
+  }
 }
 
 export default withRouter(Rows)
